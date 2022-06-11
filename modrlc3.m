b26e5f2e47922ff5aebd6ba7f3f82ea128f770e2
pab10dm <88986916+pab10dm@users.noreply.github.com>
2022-06-10 23:16:08 -0300

    Update modrlc3.m

--- modrlc3.m
+++ modrlc3.m
@@ -1,11 +1,11 @@
 %si definimos R=1k entonces RC=0.005 y C=5e-06
 %luego LC=2.933e-06 entonces L=0.5866
 function [X]=modrlc3(t_etapa, xant, accion)
-R=1000;   %resistencia R=1000
-L=0.5866;  %bobina    L=0.5866
-C=5e-6; %capacitor    C=5e-06
+C= 2.177e-05; %capacitor    C=5e-06
+L=2.347e-06/C;  %bobina    L=0.5866
+R=0.004865/C;   %resistencia R=1000
 A= [-R/L -1/L; 1/C 0]; B=[1/L; 0]; 
-h=4.5e-10;
+h=4.5e-7;
 u=accion;  %tension de entrada
 x=xant;
 for ii=1:t_etapa/h
@@ -14,7 +14,8 @@ x=x+xp*h;
 end
 X=x;
 
-%  G(s)= 1/(LCse2+RCs+1)
-%
-%  sys_G_ang=1/(2.933e-06 s^2 + 0.005 s + 1)
-%
+% la corriente
+%sys_G_ang =
+%             7.009e-07
+%   ------------------------------
+%   2.347e-06 s^2 + 0.004865 s + 1
